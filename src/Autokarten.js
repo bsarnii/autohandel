6c7742d4a1ed2fb729ae29a1c84e7524d605f4a4
bsarnii <scharnoui@gmail.com>
2022-08-18 13:00:42 +0200

    finished autokarten

--- src/Autokarten.js
+++ src/Autokarten.js
@@ -7,7 +7,51 @@ function Autokarten() {
     <div className='autokarten'>
         <h2>Unsere Fahrzeuge</h2>
         <div className='karten'>
-            <Karte />
+            <Karte 
+            name='Opel Astra'
+            img='opel-astra.jpg'
+            jahr='2016'
+            leistung='110 kW (150 PS)'
+            kraftstoff='Benzin'
+            km='65.000 km'
+            preis='12 000 €'
+            />
+            <Karte 
+            name='BMW 745e 7 series G12'
+            img='bmw-745e.jpg'
+            jahr='2020'
+            leistung='290 kW (394 PS)'
+            kraftstoff='Hybrid (Benzin/Elektro)'
+            km='3.815 km'
+            preis='85 000 €'
+             />
+            <Karte
+            name='Volkswagen Golf VII'
+            img='volkswagen-golf.jpg'
+            jahr='2019'
+            leistung='221 kW (300 PS)'
+            kraftstoff='Benzin'
+            km='57.990 km'
+            preis='34 890 €'
+            />
+            <Karte
+            name='Audi A3'
+            img='audi-a3.jpg'
+            jahr='2018'
+            leistung='110 kW (150 PS)'
+            kraftstoff='Benzin'
+            km='72.300 km'
+            preis='24 700 €'
+            />
+            <Karte
+            name='Volkswagen T-Roc R-Line'
+            img='vw-troc.jpg'
+            jahr='2020'
+            leistung='110 kW (150 PS)'
+            kraftstoff='Benzin'
+            km='6.944 km'
+            preis='42 000 €'
+            />
         </div>
     </div>
   )
--- src/Karte.js
+++ src/Karte.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import './Karte.css'
 
-function Karte() {
+function Karte({name, img, jahr, kraftstoff, leistung, km, preis}) {
   return (
     <div className='karte'>
         <img 
         className='karte-img' 
-        src='opel-astra.jpg' 
-        alt='opel astra' />
-        <div className='karte-name'>Opel Astra</div>
+        src={img}
+        alt={name} />
+        <div className='karte-name'>{name}</div>
         <div className='karte-daten'>
             <ul>
-                <li>Baujahr:2016</li>
-                <li>Leistung: 110kW(150 PS)</li>
-                <li>Kraftstoff: Benzin</li>
-                <li>km: 65.000</li>
+                <li>Baujahr: {jahr}</li>
+                <li>Leistung: {leistung}</li>
+                <li>Kraftstoff: {kraftstoff}</li>
+                <li>Kilometerstand: {km}</li>
             </ul>
         </div>
+        <div className='preis'>{preis}</div>
     </div>
   )
 }
