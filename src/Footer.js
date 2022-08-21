68b477aeb40fa97b490464e0acbc92e8bbe57025
bsarnii <scharnoui@gmail.com>
2022-08-21 14:36:52 +0200

    added footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import './Footer.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-col-1'>
+        <img src='autohaus-logo-neu-2.png' alt="firmen-logo" className='logo-footer' />
+        <p>Wir freuen uns, Ihnen bei Fragen
+          <br/> weiter helfen zu können.</p>
+      </div>
+      <div className='footer-col-2'>
+        <p>Kontakt</p>
+        <p>Hier finden Sie uns:<br />
+         Stauffenbergstraße 25, 99427<br/>
+         Weimar, Deutschland</p>
+        <p>Rufen Sie uns an:<br/>
+           01628710079</p>
+        <p>E-Mail<br/>scharnoui@gmail.com</p>  
+      </div>
+      <div className='footer-col-3'>
+        <p>Folgen Sie uns</p>
+        <div className='footer-icons'>
+          <a href='https://www.facebook.com' target='_blank' rel='noreferrer'><FacebookIcon fontSize='large'/></a>
+          <a href='https://www.twitter.com' target='_blank' rel='noreferrer'><TwitterIcon fontSize='large'/></a>
+          <a href='https://www.instagram.com' target='_blank' rel='noreferrer'><InstagramIcon fontSize='large'/></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
