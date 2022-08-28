c31ebc3a1ad3e5cb41942827de3adcd59d10bc92
bsarnii <scharnoui@gmail.com>
2022-08-28 22:06:11 +0200

    updated Home for mobile device

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -1,31 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css';
 import {Link} from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
-import { useState } from 'react';
 
 function Header({galerieClass, homeClass, kontaktClass, impressumClass}) {
-    let [trigger, setTrigger]=useState(false);
-    function triggerHamburger(){
-        if (trigger===false){
-            setTrigger(true)
+    const [toggle, setToggle] = useState(false);
+
+    function toggleHamburger(){
+        if (toggle===false){
+            setToggle(true)
         } else {
-            setTrigger(false)
+            setToggle(false)
         }
-        console.log(trigger)
     }
+
   return (
         <header>
             <div className='header-left'>
                 <img src='autohaus-logo-neu-2.png' alt="firmen-logo" className='logo' />
             </div>
-            <div className='header-right'>
+            <div className='header-right' id="primary-navigation" data-visible={toggle}>
                 <Link className={homeClass}  to='/'>Home</Link>
                 <Link className={galerieClass}  to='/galerie'>Galerie</Link>
                 <Link className={kontaktClass} to='/kontakt'>Kontakt</Link>
                 <Link className={impressumClass} to='/impressum'>Impressum</Link>
             </div>
-            <MenuIcon fontSize='large' onClick={triggerHamburger}/>
+            <MenuIcon onClick={toggleHamburger} fontSize='large' aria-controls='primary-navigation' aria-expanded='false'/>
         </header>
   )
 }
